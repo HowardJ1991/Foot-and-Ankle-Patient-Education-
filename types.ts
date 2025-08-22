2d7dbbbd75bca5fd20e80ac992ccb58671f95765
HowardJ1991 <52257383+HowardJ1991@users.noreply.github.com>
2025-08-22 11:28:31 -0600

    Delete types.ts

--- types.ts
+++
@@ -1,6 +0,0 @@
-
-import type { CATEGORIES } from './constants';
-
-export type Category = keyof typeof CATEGORIES;
-export type Topic = string;
-export type View = 'categories' | 'topics' | 'content';
